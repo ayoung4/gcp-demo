35ad78ea46e2b0edd2d7888f2e236c8afdc00707
Alfred Young <alfred@codenaturally.com>
2020-12-29 20:59:53 -0800

    node cr app added

---
+++ apps/node-cr-app/src/main.ts
@@ -0,0 +1,28 @@
+import { createServer } from 'http'
+import { PubSub } from '@google-cloud/pubsub'
+
+const pubsub = new PubSub()
+
+async function publishMessage() {
+
+  const dataBuffer = Buffer.from(JSON.stringify({ foo: 'bar' }));
+
+  try {
+    const messageId = await pubsub.topic(process.env.TOPIC_NAME).publish(dataBuffer);
+    console.log(`Message ${messageId} published.`);
+  } catch (error) {
+    console.error(`Received error while publishing: ${error.message}`);
+    process.exitCode = 1;
+  }
+
+}
+
+const server = createServer(async (req, res) => {
+  await publishMessage()
+  res.write(200)
+  res.end(200)
+})
+
+const port = process.env.PORT || 8080
+
+server.listen(port, () => console.log(`listening on ${port}`))
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/express-cr-app'],
+  projects: ['<rootDir>/apps/express-cr-app', '<rootDir>/apps/node-cr-app'],
 };
